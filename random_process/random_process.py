14cf2cdcdeddde3238511fdb103d606bff32b104
Wang Tianming <robert93inhk@gmail.com>
2016-10-28 01:02:16 +0800

    Add files via upload

--- random_process/random_process.py
+++ random_process/random_process.py
@@ -3,23 +3,26 @@ import re
 import operator
 import random
 
-num_random = 2
-limit = 1;
+num_random = 10
+limit = 20;
 
 movie_user = {} # movie_id, number of raters
 movie_popularity = {}
+movie_score = {}
+movie_score_sum = 0
 num_user = 0
 output = {}
 
 num_relevance = 0 # all movies, e per user
 num_unexpectedness = 0
 num_serendipity = 0
+num_valid_user = 0
 
 relevent = False
 unexpect = False
 
 #read in the rating database
-with open("rating_database_test.txt", "rb") as input1:
+with open("rating_database.txt", "rb") as input1:
     lines = json.loads(input1.read())
 input1.close()
 
@@ -33,12 +36,14 @@ for user_id in lines:
             movie_user[movie_id] += 1
 
 #generate average popularity rate
-sorted_movie_user = sorted(movie_user.items(), key=operator.itemgetter(1))
-average_popularity = sorted_movie_user[5055][1]
+for movie_id in movie_user:
+    movie_score[movie_id] = float(movie_user[movie_id]) / num_user
+    movie_score_sum += movie_score[movie_id]
+average_popularity = float(movie_score_sum) / len(movie_user)
 
 #genenrate popularity for each movie
 for movie_id in movie_user:
-    if float(movie_user[movie_id]) > average_popularity:
+    if float(movie_score[movie_id]) > average_popularity:
         movie_popularity[movie_id] = 1
     else:
         movie_popularity[movie_id] = 0
@@ -48,6 +53,7 @@ for user_id in lines:
     length = len(lines[user_id])
     relevance = 0.0
     if length > limit:
+        num_valid_user += 1
         sum = 0.0
         for index in lines[user_id]:
             sum = sum + float(lines[user_id][index])
@@ -58,6 +64,7 @@ for user_id in lines:
             temp = [key, rate]
             rates.append(temp)
         randoms = random.sample(range(0, length), num_random)
+        #randoms = [1,2,3,4,5,6,7,8,9,10]
         for index_selected in randoms:
             rate_temp = float(rates[index_selected][1])
             popular_temp = int(float(movie_popularity[rates[index_selected][0]]))
@@ -71,10 +78,9 @@ for user_id in lines:
                 num_serendipity = num_serendipity + 1
             relevent = False
             unexpect = False
-
-rate_relevance = float(num_relevance) / (num_random * num_user)
-rate_unexpectedness = float(num_unexpectedness) / (num_random * num_user)
-rate_serendipitous = float(num_serendipity) / (num_random * num_user)
+rate_relevance = float(num_relevance) / (num_valid_user * num_random)
+rate_unexpectedness = float(num_unexpectedness) / (num_valid_user * num_random)
+rate_serendipitous = float(num_serendipity) / (num_valid_user * num_random)
 result = [rate_relevance, rate_unexpectedness, rate_serendipitous]
 
 with open("rating_result.txt", "wb") as output1:
